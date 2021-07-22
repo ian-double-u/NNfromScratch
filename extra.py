365d763601d861c109130b02a3574384859e2ca7
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2021-07-22 18:12:12 -0400

    Delete extra.py

--- extra.py
+++
@@ -1,6 +0,0 @@
-from math import exp
-import numpy as np
-
-activation_function = lambda x: (exp(x))/(exp(x)+1)
-cost_function = lambda out, ex: sum(np.square(np.subtract(out,ex)))
-rule_function = lambda output: output.index(max(output)[0])
