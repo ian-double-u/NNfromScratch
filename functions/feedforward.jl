f9041b50926f7ce478025f24f6cccc88ed49e61d
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2021-07-25 09:35:03 -0400

    Update feedforward.jl

--- functions/feedforward.jl
+++ functions/feedforward.jl
@@ -1,8 +1,10 @@
 function feedforward!(net::Network,input::Vector{Float64})::Vector{Float64}
     net.layers[1] = input
 
-    for i=2:length(net.layers)
-        net.neurons[i+1] = (net.weights[i-1]*net.neurons[i] + net.biases[i-1]).net.act()
+    # a^(l+1) = σ(w^l*a^l + b^1)
+    for i=1:(length(net.layers)-1)
+        net.z[i] = net.weights[i]*net.neurons[i] + net.biases[i]
+        net.neurons[i+1] = net.z[i].net.act()
     end
 
     return net.neurons[end]
