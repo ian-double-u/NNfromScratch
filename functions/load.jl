ab38054f0ab1a4c9f2a79b1ade674b91ee4340e7
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2021-07-25 11:37:37 -0400

    Create load.jl

---
+++ functions/load.jl
@@ -0,0 +1,13 @@
+function load!(net::Network,file_str::String)::Network
+    dict = Dict()
+
+    open(file_str, "r") do file
+        global dict
+        dict = JSON.parse(file)
+    end
+
+    net.weights = dict["weights"]
+    net.biases = dict["biases"]
+
+    return net
+end
