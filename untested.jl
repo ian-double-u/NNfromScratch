3392fcd9509c96dbd8c267bf7fc3a952bc586dd9
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2021-07-25 15:14:32 -0400

    Update untested.jl

--- untested.jl
+++ untested.jl
@@ -118,3 +118,60 @@ function load!(net::Network,file_str::String)::Network
 
     return net
 end
+
+function sigmoid(x::Float64)::Float64
+    return (MathConstants.e^x)/(1 + MathConstants.e^x)
+end
+
+function sigmoid_prime(x::Float64)::Float64
+    return (MathConstants.e^x)/(1 + 2MathConstants.e^x + MathConstants.e^(2x))
+end
+
+function MSE(a::Vector{Float64},l::Vector{Float64})::Float64
+    return 0.5*(sum((a.-l).^2))
+end
+
+function MSE_p(a::Vector{Float64},l::Vector{Float64})::Float64
+    return sum(a.-l)
+end
+
+function mnist_load(file_str::String)::Tuple
+    dict = Dict()
+
+    open(file_str, "r") do file
+        global dict
+        dict = JSON.parse(file)
+    end
+
+    # <- ::Vector{Tuple{Vector{String},String}}
+    train = dict["training"]
+    valid = dict["validation"]
+    test = dict["test"]
+
+    # -> ::Vector{Tuple{Vector{Float64},Float64}}
+    for t in train
+        t[1] = parse(Float64,t[1])
+
+        for s in t[0]
+            s = parse(Float64,s)
+        end
+    end
+
+    for t in valid
+        t[1] = parse(Float64,t[1])
+
+        for s in t[0]
+            s = parse(Float64,s)
+        end
+    end
+
+    for t in test
+        t[1] = parse(Float64,t[1])
+
+        for s in t[0]
+            s = parse(Float64,s)
+        end
+    end
+
+    return (train,valid,test)
+end
