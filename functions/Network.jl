54827a30efffd48a394ed90975448968c0b1dd44
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2021-07-25 09:35:56 -0400

    Create Network.jl

---
+++ functions/Network.jl
@@ -0,0 +1,17 @@
+Network()
+    layers::Vector{Int64}
+    neurons::Vector{Vector{Float64}}
+    weights::Vector{Matrix{Float64}}
+    biases::Vector{Vector{Float64}}
+    z::Vector{Vector{Float64}}
+    batch::Int64
+    epoch::Int64
+    η::Float64
+    act::Function
+    act_p::Function
+    C::Function
+    C_p::Function
+    ∇C_a(a,l)::Fucntion #takes activation layer a, label l
+
+    # __constructor__ (use randn(Float64,n,m))
+end
