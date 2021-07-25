5ec49e52dbd00a99a78e63b26a05eaedb1b82ebb
Ian Wehba <56119986+ian-double-u@users.noreply.github.com>
2021-07-25 08:58:59 -0400

    Update feedforward.jl

--- functions/feedforward.jl
+++ functions/feedforward.jl
@@ -1,8 +1,8 @@
-function feedforward!(net::Network,act::Function,input::Vector{Float64})::Vector{Float64}
+function feedforward!(net::Network,input::Vector{Float64})::Vector{Float64}
     net.layers[1] = input
 
     for i=2:length(net.layers)
-        net.neurons[i+1] = (net.weights[i-1]*net.neurons[i] + net.biases[i-1]).act()
+        net.neurons[i+1] = (net.weights[i-1]*net.neurons[i] + net.biases[i-1]).net.act()
     end
 
     return net.neurons[end]
